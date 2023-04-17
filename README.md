737113698135a7fb29daa914a47da8f2e9b6384e
Peter Reuterås <peter@reuteras.com>
2023-04-17 12:57:18 +0200

    Add support for JSON

--- README.md
+++ README.md
@@ -8,9 +8,13 @@ A small tool to generate an [RSS][rss] feed from some [Wordpress][wor] blogs tha
 pip install rssfixer
 ```
 
-## Usage
+## Example
 
-An example to generate a feed for [nccgroup][ncc]:
+Format for storing the links varies but I'll try and add more formats as I find them.
+
+### List
+
+An example to generate a feed for [nccgroup][ncc] that have the links in a list:
 
 ```bash
 $ rssfixer --title nccgroup https://research.nccgroup.com/
@@ -31,7 +35,44 @@ Most times you would run the script from crontab to have an updated feed. Here i
 32 * * * *      /home/user/src/rssfixer/bin/rssfixer --title nccgroup --output /var/www/html/feeds/nccgroup.xml --quiet https://research.nccgroup.com
 ```
 
+### JSON
+
+An example for [truesec.com][tru]:
+
+```bash
+rssfixer --json --quiet --output truesex.xml https://www.truesec.com/hub/blog
+```
+
+### Usage
+
+```bash
+usage: rssfixer [-h] [--json] [--json-entries JSON_ENTRIES] [--json-url JSON_URL] [--json-title JSON_TITLE] [--json-description JSON_DESCRIPTION] [--output OUTPUT]
+                [--title TITLE] [-q]
+                url
+
+Generate RSS feed for blog that don't publish a feed
+
+positional arguments:
+  url                   URL for the blog
+
+options:
+  -h, --help            show this help message and exit
+  --json                Find entries in JSON
+  --json-entries JSON_ENTRIES
+                        JSON key for entries (default: 'entries')
+  --json-url JSON_URL   JSON key for URL (default: 'url')
+  --json-title JSON_TITLE
+                        JSON key for title (default: 'title')
+  --json-description JSON_DESCRIPTION
+                        JSON key for description (default: 'preamble')
+  --output OUTPUT       Name of the output file (default: "rss_feed.xml")
+  --title TITLE         Title of the RSS feed (default: "My RSS Feed")
+  -q, --quiet           Suppress output
+```
+
+
   [exa]: https://github.com/reuteras/rssfixer/blob/main/example/nccgroup.xml
   [ncc]: https://research.nccgroup.com/
   [rss]: https://www.rssboard.org/
+  [tru]: https://www.truesec.com/hub/blog
   [wor]: https://wordpress.org/
--- src/rssfixer/rss.py
+++ src/rssfixer/rss.py
@@ -2,6 +2,7 @@
 import argparse
 from bs4 import BeautifulSoup
 from feedgen.feed import FeedGenerator
+import json
 import requests
 import sys
 
@@ -14,8 +15,25 @@ def fetch_html(url):
         sys.exit(1)
     return response.text
 
+def find_entries(json_object, entries_key):
+    """Find the entries in a JSON object with key "entries_key"."""
+    if isinstance(json_object, dict):
+        for key, value in json_object.items():
+            if key == entries_key:
+                return value
+            else:
+                result = find_entries(value, entries_key)
+                if result is not None:
+                    return result
+    elif isinstance(json_object, list):
+        for item in json_object:
+            result = find_entries(item, entries_key)
+            if result is not None:
+                return result
+    return None
+
 def extract_links(soup):
-    """Extract links from an HTML page."""
+    """Extract links from an HTML page with links in <ul>-lists."""
     links = []
     unique_links = set()
 
@@ -26,14 +44,49 @@ def extract_links(soup):
             if link:
                 url = link['href']
                 title = link.text.strip()
+                description = link.text.strip()
 
                 # Exclude URLs containing '/category/' or '/author/'
                 if '/category/' not in url and '/author/' not in url:
                     # Check if the URL is unique
                     if url not in unique_links:
                         unique_links.add(url)
-                        links.append((url, title))
+                        links.append((url, title, description))
 
+    if links == []:
+        print("ERROR: No links found")
+        sys.exit(1)
+    return links
+
+def extract_links_json(soup, arguments):
+    """Extract links from JSON strings in an HTML page."""
+    links = []
+    unique_links = set()
+
+    # Find the JSON string in the page
+    for json_text in soup.find_all('script', type="application/json"):
+        json_object = json.loads(json_text.text)
+        entries = find_entries(json_object, arguments.json_entries)
+        if entries is not None:
+            break
+
+    # Extract the links from the JSON object
+    for entry in entries:
+        try:
+            url = entry[arguments.json_url]
+            title = entry[arguments.json_title]
+        except KeyError:
+            print("ERROR: Unable to find URL or title in JSON object")
+            sys.exit(1)
+        try:
+            description = entry[arguments.json_description]
+        except KeyError:
+            # Ignore description if it's not found
+            description = ""
+        if url not in unique_links:
+            unique_links.add(url)
+            links.append((url, title, description))
+    
     if links == []:
         print("ERROR: No links found")
         sys.exit(1)
@@ -47,26 +100,36 @@ def create_rss_feed(links, html_url, feed_title, feed_description):
     fg.description(feed_description)
     fg.link(href=html_url, rel="alternate")
 
-    for url, title in links:
+    for url, title, description in links:
         fe = fg.add_entry()
         fe.id(url)
         fe.title(title)
         fe.link(href=url)
+        fe.description(description)
 
     return fg.rss_str(pretty=True).decode("utf-8")
 
 def main():
     """Main function."""
-    parser = argparse.ArgumentParser(description="Convert an HTML list of links to an RSS feed")
-    parser.add_argument("url", help="URL of the HTML list of links")
-    parser.add_argument("--title", default="My RSS Feed", help='Title of the RSS feed (default: "My RSS Feed")')
+    parser = argparse.ArgumentParser(description="Generate RSS feed for blog that don't publish a feed")
+    parser.add_argument("url", help="URL for the blog")
+    parser.add_argument("--json", action="store_true", help="Find entries in JSON")
+    parser.add_argument("--json-entries", default="entries", help="JSON key for entries (default: 'entries')")
+    parser.add_argument("--json-url", default="url", help="JSON key for URL (default: 'url')")
+    parser.add_argument("--json-title", default="title", help="JSON key for title (default: 'title')")
+    parser.add_argument("--json-description", default="preamble", help="JSON key for description (default: 'preamble')")
     parser.add_argument("--output", default="rss_feed.xml", help='Name of the output file (default: "rss_feed.xml")')
+    parser.add_argument("--title", default="My RSS Feed", help='Title of the RSS feed (default: "My RSS Feed")')
     parser.add_argument("-q", "--quiet", action="store_true", help="Suppress output")
     args = parser.parse_args()
 
     html_content = fetch_html(args.url)
     soup = BeautifulSoup(html_content, "html.parser")
-    links = extract_links(soup)
+    if args.json:
+        links = extract_links_json(soup, args)
+    else:
+        links = extract_links(soup)
+
     feed_description = f"RSS feed generated from the links at {args.url}"
     rss_feed = create_rss_feed(links, args.url, args.title, feed_description)
 
