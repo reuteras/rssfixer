4de515f639ca564834228894ab4c85b63ffe0242
Peter Reuterås <peter@reuteras.com>
2023-04-19 12:59:46 +0200

    Add general HTML option.

--- src/rssfixer/rss.py
+++ src/rssfixer/rss.py
@@ -1,6 +1,8 @@
-"""Generate rss feed from "blogs" without rss feed."""
+"""Generate rss feed for "blogs" without rss feed."""
 import argparse
 import json
+import pkg_resources
+import re
 import sys
 
 import requests
@@ -57,7 +59,35 @@ def extract_links_ul(soup):
                         unique_links.add(url)
                         links.append((url, title, description))
 
-    if links == []:
+    if not links:
+        print("ERROR: No links found")
+        sys.exit(1)
+    return links
+
+
+def extract_links_html(soup, arguments):
+    """Extract links from an HTML page with links in selectable elements."""
+    links = []
+    unique_links = set()
+
+    # Iterate through all the elements of type html_entries in the page
+    for entry in soup.find_all(arguments.html_entries):
+        try:
+            url = entry.findNext(arguments.html_url)["href"]
+            title = entry.find(arguments.html_title, re.compile(arguments.html_title_class)).text.strip()
+        except (KeyError, AttributeError):
+            print("ERROR: Unable to find URL or title in HTML element")
+            sys.exit(1)
+        try:
+            description = entry.find(arguments.html_description, re.compile(arguments.html_description_class)).text.strip()
+        except (KeyError, AttributeError):
+            # Ignore description if it's not found
+            description = ""
+        if url not in unique_links:
+            unique_links.add(url)
+            links.append((url, title, description))
+
+    if not links:
         print("ERROR: No links found")
         sys.exit(1)
     return links
@@ -75,22 +105,22 @@ def extract_links_json(soup, arguments):
         if entries is not None:
             break
 
-    # Extract the links from the JSON object
-    for entry in entries:
-        try:
-            url = entry[arguments.json_url]
-            title = entry[arguments.json_title]
-        except KeyError:
-            print("ERROR: Unable to find URL or title in JSON object")
-            sys.exit(1)
-        try:
-            description = entry[arguments.json_description]
-        except KeyError:
-            # Ignore description if it's not found
-            description = ""
-        if url not in unique_links:
-            unique_links.add(url)
-            links.append((url, title, description))
+        # Extract the links from the JSON object
+        for entry in entries:
+            try:
+                url = entry[arguments.json_url]
+                title = entry[arguments.json_title]
+            except KeyError:
+                print("ERROR: Unable to find URL or title in JSON object")
+                sys.exit(1)
+            try:
+                description = entry[arguments.json_description]
+            except KeyError:
+                # Ignore description if it's not found
+                description = ""
+            if url not in unique_links:
+                unique_links.add(url)
+                links.append((url, title, description))
 
     if links == []:
         print("ERROR: No links found")
@@ -98,30 +128,51 @@ def extract_links_json(soup, arguments):
     return links
 
 
-def create_rss_feed(links, html_url, feed_title, feed_description):
+def create_rss_feed(links, arguments):
     """Create an RSS feed from a list of links."""
-    fg = FeedGenerator()
-    fg.id(html_url)
-    fg.title(feed_title)
-    fg.description(feed_description)
-    fg.link(href=html_url, rel="alternate")
+    feed_description = f"RSS feed generated from the links at {arguments.url}"
 
+    # Create the RSS feed
+    fg = FeedGenerator()
+    fg.id(arguments.url)
+    fg.title(arguments.title)
+    fg.description(feed_description)
+    fg.link(href=arguments.url, rel="alternate")
+
+    # Add entries to the RSS feed
     for url, title, description in links:
+        if arguments.base_url:
+            url = arguments.base_url + url
         fe = fg.add_entry()
         fe.id(url)
         fe.title(title)
         fe.link(href=url)
         fe.description(description)
 
-    return fg.rss_str(pretty=True).decode("utf-8")
+    if vars(arguments).get("atom", False):
+        return fg.atom_str(pretty=True).decode("utf-8")
+    else:
+        return fg.rss_str(pretty=True).decode("utf-8")
 
 
-def main():
-    """Main function."""
+def parse_arguments():
+    """Parse command line arguments."""
     parser = argparse.ArgumentParser(
-        description="Generate RSS feed for blog that don't publish a feed"
+        description="""Generate RSS feed for blog that don't publish a feed.
+        Default is to find links in a simple <ul>-list. 
+        Options are available to find links in other HTML elements or JSON strings."""
     )
+    parser.add_argument("--version", action="version", version="%(prog)s " + pkg_resources.get_distribution("rssfixer").version)
     parser.add_argument("url", help="URL for the blog")
+    parser.add_argument("--atom", action="store_true", help="Generate Atom feed")
+    parser.add_argument("--base-url", help="Base URL for the blog")
+    parser.add_argument("--html", action="store_true", help="Find entries in HTML")
+    parser.add_argument("--html-entries", default="article", help="HTML selector for entries")
+    parser.add_argument("--html-url", default="a", help="HTML selector for URL")
+    parser.add_argument("--html-title", default="h3", help="HTML selector for title")
+    parser.add_argument("--html-title-class", default="title", help="Flag to specify title class (regex)")
+    parser.add_argument("--html-description", default="div", help="HTML selector for description")
+    parser.add_argument("--html-description-class", default="summary" ,help="Flag to specify description class (regex)")
     parser.add_argument("--json", action="store_true", help="Find entries in JSON")
     parser.add_argument(
         "--json-entries",
@@ -132,17 +183,17 @@ def main():
         "--json-url", default="url", help="JSON key for URL (default: 'url')"
     )
     parser.add_argument(
-        "--json-title", default="title", help="JSON key for title (default: 'title')"
+        "--json-title", default="title", help="JSON key for title"
     )
     parser.add_argument(
         "--json-description",
         default="preamble",
-        help='JSON key for description (default: "preamble")',
+        help='JSON key for description',
     )
     parser.add_argument(
         "--output",
         default="rss_feed.xml",
-        help='Name of the output file (default: "rss_feed.xml")',
+        help='Name of the output file',
     )
     parser.add_argument(
         "--title",
@@ -150,27 +201,51 @@ def main():
         help='Title of the RSS feed (default: "My RSS Feed")',
     )
     parser.add_argument("-q", "--quiet", action="store_true", help="Suppress output")
-    args = parser.parse_args()
+    parser.add_argument("--list", action="store_true", help="Find entries in HTML <ul>-list (default)")
 
-    html_content = fetch_html(args.url)
-    soup = BeautifulSoup(html_content, "html.parser")
-    if args.json:
-        links = extract_links_json(soup, args)
-    else:
-        links = extract_links_ul(soup)
+    return parser
 
-    feed_description = f"RSS feed generated from the links at {args.url}"
-    rss_feed = create_rss_feed(links, args.url, args.title, feed_description)
-
+def save_rss_feed(rss_feed, arguments):
+    """Save the RSS feed to a file."""
     try:
-        with open(args.output, "w", encoding="utf-8") as f:
+        with open(arguments.output, "w", encoding="utf-8") as f:
             f.write(rss_feed)
     except IOError:
         print("ERROR: Unable to write to file")
         sys.exit(1)
+    
+    if not arguments.quiet:
+        if arguments.atom:
+            print(f"Atom feed created: {arguments.output}")
+        else:
+            print(f"RSS feed created: {arguments.output}")
 
-    if not args.quiet:
-        print(f"RSS feed created: {args.output}")
+def main():
+    """Main function."""
+    parsed_args = parse_arguments()
+    args = parsed_args.parse_args()
+
+    if vars(args).get("version"):
+        print(pkg_resources.require("rssfixer")[0].version)
+        sys.exit(0)
+
+    # Get HTML content from URL
+    html_content = fetch_html(args.url)
+    soup = BeautifulSoup(html_content, "html.parser")
+
+    # Select function to handle different types of blogs
+    if args.json:
+        links = extract_links_json(soup, args)
+    elif args.html:
+        links = extract_links_html(soup, args)
+    elif args.list:
+        links = extract_links_ul(soup)
+    else:
+        links = extract_links_ul(soup)
+        
+    # Create RSS feed and save to file
+    rss_feed = create_rss_feed(links, args)
+    save_rss_feed(rss_feed, args)
 
 
 if __name__ == "__main__":
