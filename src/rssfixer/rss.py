966cb9430ac0f910125d495fe6ff5aa47da3e82c
Peter Reuterås <reuteras@users.noreply.github.com>
2023-04-19 13:40:02 +0200

    Fix linting

--- src/rssfixer/rss.py
+++ src/rssfixer/rss.py
@@ -1,10 +1,10 @@
 """Generate rss feed for "blogs" without rss feed."""
 import argparse
 import json
-import pkg_resources
 import re
 import sys
 
+import pkg_resources
 import requests
 from bs4 import BeautifulSoup
 from feedgen.feed import FeedGenerator
@@ -74,12 +74,16 @@ def extract_links_html(soup, arguments):
     for entry in soup.find_all(arguments.html_entries):
         try:
             url = entry.findNext(arguments.html_url)["href"]
-            title = entry.find(arguments.html_title, re.compile(arguments.html_title_class)).text.strip()
+            title = entry.find(
+                arguments.html_title, re.compile(arguments.html_title_class)
+            ).text.strip()
         except (KeyError, AttributeError):
             print("ERROR: Unable to find URL or title in HTML element")
             sys.exit(1)
         try:
-            description = entry.find(arguments.html_description, re.compile(arguments.html_description_class)).text.strip()
+            description = entry.find(
+                arguments.html_description, re.compile(arguments.html_description_class)
+            ).text.strip()
         except (KeyError, AttributeError):
             # Ignore description if it's not found
             description = ""
@@ -170,9 +174,21 @@ def parse_arguments():
     parser.add_argument("--html-entries", default="article", help="HTML selector for entries")
     parser.add_argument("--html-url", default="a", help="HTML selector for URL")
     parser.add_argument("--html-title", default="h3", help="HTML selector for title")
-    parser.add_argument("--html-title-class", default="title", help="Flag to specify title class (regex)")
-    parser.add_argument("--html-description", default="div", help="HTML selector for description")
-    parser.add_argument("--html-description-class", default="summary" ,help="Flag to specify description class (regex)")
+    parser.add_argument(
+        "--html-title-class",
+        default="title",
+        help="Flag to specify title class (regex)",
+    )
+    parser.add_argument(
+        "--html-description",
+        default="div",
+        help="HTML selector for description",
+    )
+    parser.add_argument(
+        "--html-description-class",
+        default="summary",
+        help="Flag to specify description class (regex)",
+    )
     parser.add_argument("--json", action="store_true", help="Find entries in JSON")
     parser.add_argument(
         "--json-entries",
@@ -182,18 +198,16 @@ def parse_arguments():
     parser.add_argument(
         "--json-url", default="url", help="JSON key for URL (default: 'url')"
     )
-    parser.add_argument(
-        "--json-title", default="title", help="JSON key for title"
-    )
+    parser.add_argument("--json-title", default="title", help="JSON key for title",)
     parser.add_argument(
         "--json-description",
         default="preamble",
-        help='JSON key for description',
+        help="JSON key for description",
     )
     parser.add_argument(
         "--output",
         default="rss_feed.xml",
-        help='Name of the output file',
+        help="Name of the output file",
     )
     parser.add_argument(
         "--title",
@@ -201,9 +215,12 @@ def parse_arguments():
         help='Title of the RSS feed (default: "My RSS Feed")',
     )
     parser.add_argument("-q", "--quiet", action="store_true", help="Suppress output")
-    parser.add_argument("--list", action="store_true", help="Find entries in HTML <ul>-list (default)")
+    parser.add_argument(
+        "--list", action="store_true", help="Find entries in HTML <ul>-list (default)"
+    )
 
     return parser
+
 
 def save_rss_feed(rss_feed, arguments):
     """Save the RSS feed to a file."""
@@ -213,12 +230,13 @@ def save_rss_feed(rss_feed, arguments):
     except IOError:
         print("ERROR: Unable to write to file")
         sys.exit(1)
-    
+
     if not arguments.quiet:
         if arguments.atom:
             print(f"Atom feed created: {arguments.output}")
         else:
             print(f"RSS feed created: {arguments.output}")
+
 
 def main():
     """Main function."""
@@ -242,7 +260,7 @@ def main():
         links = extract_links_ul(soup)
     else:
         links = extract_links_ul(soup)
-        
+
     # Create RSS feed and save to file
     rss_feed = create_rss_feed(links, args)
     save_rss_feed(rss_feed, args)
