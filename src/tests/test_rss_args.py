7865a11d51664941e6decb4cdd2186effec9fdd7
Peter Reuterås <peter@reuteras.com>
2023-04-26 12:13:02 +0200

    Add and update tests

--- src/tests/test_rss_args.py
+++ src/tests/test_rss_args.py
@@ -28,3 +28,52 @@ def test_rss_args_json_html_entries():
             ]
         )
     assert pytest_wrapped_e.value.code == 2
+
+
+def test_parse_arguments_required_args():
+    arguments = ["--list", "https://example.com"]
+    args = rss.parse_arguments(arguments)
+    assert args.list
+    assert args.url == "https://example.com"
+
+def test_parse_arguments_optional_args():
+    arguments = [
+        "--html",
+        "--html-entries", "div.entry",
+        "--html-url", "a.url",
+        "--html-title", "h2.title",
+        "--html-title-class", "custom-title",
+        "--html-description", "div.description",
+        "--html-description-class", "custom-description",
+        "https://example.com",
+    ]
+    args = rss.parse_arguments(arguments)
+    assert args.html
+    assert args.html_entries == "div.entry"
+    assert args.html_url == "a.url"
+    assert args.html_title == "h2.title"
+    assert args.html_title_class == "custom-title"
+    assert args.html_description == "div.description"
+    assert args.html_description_class == "custom-description"
+    assert args.url == "https://example.com"
+
+def test_parse_arguments_json_args():
+    arguments = [
+        "--json",
+        "--json-entries", "items",
+        "--json-url", "link",
+        "--json-title", "name",
+        "--json-description", "summary",
+        "https://example.com",
+    ]
+    args = rss.parse_arguments(arguments)
+    assert args.json
+    assert args.json_entries == "items"
+    assert args.json_url == "link"
+    assert args.json_title == "name"
+    assert args.json_description == "summary"
+    assert args.url == "https://example.com"
+
+def test_parse_arguments_invalid_args():
+    with pytest.raises(SystemExit):
+        rss.parse_arguments(["--html", "--json", "https://example.com"])
---
+++ src/tests/test_rss_main.py
@@ -0,0 +1,67 @@
+import re
+
+from rssfixer import rss
+
+
+def test_main_link(capsys, requests_mock):
+    """Test the main function."""
+    url = "https://research.nccgroup.com/"
+    with open("src/tests/data/input/nccgroup.html", "r", encoding="utf-8") as f:
+        source = f.read()
+    with open("src/tests/data/output/nccgroup.xml", "r", encoding="utf-8") as f:
+        result = f.read()
+    requests_mock.get(url, text=source)
+    args = ["--title", "nccgroup", "--stdout", "--list", "https://research.nccgroup.com/"]
+
+    rss.main(args)
+    captured = re.sub(
+        r"<lastBuildDate>.*</lastBuildDate>",
+        "<lastBuildDate>Fri, 21 Apr 2023 12:15:48 +0000</lastBuildDate>",
+        capsys.readouterr().out,
+    )[:-1]
+    assert captured == result
+
+
+def test_main_json(capsys, requests_mock):
+    """Test the main function with json arguments."""
+    url = "https://security.apple.com/blog"
+    with open("src/tests/data/input/apple.html", "r", encoding="utf-8") as f:
+        source = f.read()
+    with open("src/tests/data/output/apple.xml", "r", encoding="utf-8") as f:
+        result = f.read()
+    requests_mock.get(url, text=source)
+    arguments = [
+                "--title",
+                "Apple Security",
+                "--atom",
+                "--json",
+                "--json-entries",
+                "blogs",
+                "--json-url",
+                "slug",
+                "--stdout",
+                "--quiet",
+                "--base-url",
+                "https://security.apple.com/blog/",
+                "https://security.apple.com/blog",
+            ]
+    rss.main(arguments)
+    captured = re.sub(
+        r"<updated>.*</updated>",
+        "<updated>TIME</updated>",
+        capsys.readouterr().out,
+    )[:-1]
+    result = re.sub(
+        r"<updated>.*</updated>",
+        "<updated>TIME</updated>",
+        result,
+    )
+    args = rss.parse_arguments(arguments)
+
+    assert captured == result
+    assert args.json
+    assert args.json_entries == "blogs"
+    assert args.json_url == "slug"
+    assert args.json_title == "title"
+    assert args.json_description == "description"
+    assert args.url == "https://security.apple.com/blog"
